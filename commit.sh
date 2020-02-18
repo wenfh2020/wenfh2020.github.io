54425d922e86bb010ca3e885be28e9f7443fb475
wenfh2020 <wenfh2020@126.com>
2020-02-18 09:46:44 +0800

    更新脚本支持空格

--- commit.sh
+++ commit.sh
@@ -6,9 +6,9 @@ cd `dirname $0`
 work_path=`pwd`
 cd $work_path
 
-if [ $# != 1 ]; then
+if [ $# == 0 ]; then
     echo 'pls input commit message'
     exit 1
 fi
 
-git add _posts && git commit -m $1 && git push -u origin master
+git add _posts && git commit -m $@ && git push -u origin master
