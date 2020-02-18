c73c778ad430c8d8101de04bc537dfa69efd805a
wenfh2020 <wenfh2020@126.com>
2020-02-18 10:50:56 +0800

    update shell commit

--- commit.sh
+++ commit.sh
@@ -1,17 +1,8 @@
-# wenfh2020/2020-02-18/auto commit to github
-
 #!/bin/sh
+# wenfh2020/2020-02-18/auto commit to github
 
 cd `dirname $0`
 work_path=`pwd`
 cd $work_path
 
-if [ $# == 0 ]; then
-    echo 'pls input commit message'
-    exit 1
-fi
-
-str="'"
-message=$str$@$str
-
-git add _posts && git commit -m $message && git push -u origin master
+git pull && git add _posts && git commit -m "$(echo "$@")" && git push -u origin master
