13168426662d6c83669b59cd953b925729541aca
wenfh2020 <wenfh2020@126.com>
2020-02-18 11:28:50 +0800

    updaet shell

--- commit.sh
+++ commit.sh
@@ -5,4 +5,12 @@ cd `dirname $0`
 work_path=`pwd`
 cd $work_path
 
+if [ $# -lt 1 ]; then
+    echo 'pls input commit message'
+    exit 1
+fi
+
+echo $@
+exit 1
+
 git pull && git add _posts && git commit -m "$(echo "$@")" && git push -u origin master
