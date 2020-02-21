498788c6786a7f0b8a1e504b40f691971961e1d7
wenfh2020 <wenfh2020@126.com>
2020-02-21 08:22:55 +0800

    update blog

--- refresh.sh
+++ refresh.sh
@@ -1,10 +1,16 @@
-# wenfh2020/2020-02-18/refresh for browser
-
 #!/bin/sh
+# wenfh2020/2020-02-18/refresh for browser
 
 cd `dirname $0`
 work_path=`pwd`
 cd $work_path
 
-[ -d _site ] &&  rm -r _site
-jekyll s --incremental
+# find and kill
+_pids=`ps -ef | grep 'jekyll serve' | grep -v grep | awk '{print $2}'`
+for p in $_pids
+do
+    echo "kill pid=$p"
+    kill -9 $p
+done
+
+jekyll serve -wIt
