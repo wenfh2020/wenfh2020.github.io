0289e3e479bf7682e25ce917f37c4693077eee38
wenfh2020 <wenfh2020@126.com>
2023-12-23 11:59:17 +0800

    update script

--- auto_commit.py
+++ auto_commit.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
 import os
 import time
 import sys
@@ -5,12 +8,10 @@ import sys
 my_path = r'E:\work\myself\wenfh2020.github.io'
 
 
-def git_auto_commit(commit_message, repo_path='.', is_push=True):
+def git_auto_command(repo_path='.', is_push=True):
     os.chdir(repo_path)
     try:
         if is_push:
-            # os.system('git add .')
-            # os.system('git commit -m "{}"'.format(commit_message))
             if os.system('git push') != 0:
                 return False
         else:
@@ -31,11 +32,8 @@ if len(sys.argv) > 1:
     is_push = (cmd == 'push')
 
 while True:
-    if not git_auto_commit("auto commit", my_path, is_push):
+    if not git_auto_command(my_path, is_push):
         time.sleep(1)
     else:
-        if is_push:
-            print("push done!")
-        else:
-            print("pull done!")
+        print("push done!") if is_push else print("pull done!")
         break
