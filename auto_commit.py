6a14df5726adbfcedf2dedb53cd65009019c2b61
wenfh2020 <wenfh2020@126.com>
2023-12-23 11:53:59 +0800

    update script

--- auto_commit.py
+++ auto_commit.py
@@ -1,24 +1,41 @@
 import os
 import time
+import sys
+
+my_path = r'E:\work\myself\wenfh2020.github.io'
 
 
-def git_auto_commit(commit_message, repo_path='.'):
+def git_auto_commit(commit_message, repo_path='.', is_push=True):
     os.chdir(repo_path)
     try:
-        # os.system('git add .')
-        # os.system('git commit -m "{}"'.format(commit_message))
-        if os.system('git push') != 0:
-            return False
+        if is_push:
+            # os.system('git add .')
+            # os.system('git commit -m "{}"'.format(commit_message))
+            if os.system('git push') != 0:
+                return False
+        else:
+            if os.system('git pull') != 0:
+                return False
     except Exception as e:
-        print("出现错误: ", str(e))
+        print("error: ", str(e))
         return False
     return True
 
 
+is_push = True
+if len(sys.argv) > 1:
+    cmd = sys.argv[1]
+    if cmd != 'push' and cmd != 'pull':
+        print("pls input: auto_commit [push/pull]")
+        exit(1)
+    is_push = (cmd == 'push')
+
 while True:
-    r = git_auto_commit("自动提交", r'E:\work\myself\wenfh2020.github.io')
-    if not r:
+    if not git_auto_commit("auto commit", my_path, is_push):
         time.sleep(1)
     else:
-        print("done!")
+        if is_push:
+            print("push done!")
+        else:
+            print("pull done!")
         break
